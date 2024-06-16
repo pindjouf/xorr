4e72950e90588c6925c288f509364fe3b96e44f9
pindjouf <esaubukasa@protonmail.com>
2024-06-16 23:34:42 +0200

    just something to compare

--- xor.py
+++ xor.py
@@ -1,47 +1,16 @@
-import os
-
-# Get user input
-text = input("Gimme some input: ")
-key = input("Gimme a key: ")
-
-# Define the values
 plaintext = []
-secret_key = []
-ciphertext = []
 
-# Put user input in list
-for i in text:
-    plaintext.append(int(i))
+with open("./yo.txt", "r") as f:
+    file = f.read()
+    for i in file:
+        print(i)
+        binary = bin(ord(i))[2:]
+        plaintext.append(binary)
 
-for i in key:
-    secret_key.append(int(i))
+for i in range(len(plaintext)):
+    while len(plaintext[i]) < 8:
+        plaintext[i] = str(0) + plaintext[i]
 
-# Make list length equal for operation
-if len(plaintext) < len(secret_key):
-    result = len(secret_key) - len(plaintext)
-    for i in range(result):
-        plaintext.insert(0, 0)
-
-elif len(secret_key) < len(plaintext):
-    result = len(plaintext) - len(plaintext)
-    for i in range(result):
-        secret_key.insert(0, 0)
-
-# Get ciphertext from XOR operation
-for x,y in zip(plaintext, secret_key):
-    if x == y:
-        ciphertext.append(0)
-    else:
-        ciphertext.append(1)
-
-# Make ciphertext pretty
-pretty_ci = ''.join(map(str, ciphertext))
-
-# Convert bits to decimal
-deci_ciphertext = int(''.join(map(str, ciphertext)), 2)
-
-# Clear screen pre-print
-# os.system('clear')
-
-print(f"The ciphertext is: {pretty_ci}")
-print(f"Decimal value: {deci_ciphertext}")
+for elem in plaintext:
+    deci = int(''.join(map(str, elem)), 2)
+    print(deci)
